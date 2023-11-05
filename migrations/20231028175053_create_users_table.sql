93768cf5f01eeaf216837d5749c22c961e623376
blazee5 <vladislavkutsenko742@gmail.com>
2023-11-05 17:22:42 +0600

    fix user migration

--- migrations/20231028175053_create_users_table.sql
+++ migrations/20231028175053_create_users_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     fio VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    avatar TEXT
+    avatar VARCHAR(255) DEFAULT ''
 );
 -- +goose StatementEnd
 
