4e48f6efcd79d802ed82f5596e2ad04d7341eba5
blazee5 <vladislavkutsenko742@gmail.com>
2023-11-23 20:07:24 +0600

    test

--- lib/db/postgres/postgres.go
+++ lib/db/postgres/postgres.go
@@ -9,8 +9,8 @@ import (
 )
 
 func New() *sqlx.DB {
-	db, err := sqlx.Connect("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME")))
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL")))
 
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
