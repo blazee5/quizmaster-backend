1554badad5ec9bc11ba2db2eeb2b72e37e32cdb7
blazee5 <vladislavkutsenko742@gmail.com>
2023-11-27 23:10:21 +0600

    change created question order_id to 0

--- internal/question/pg_repository.go
+++ internal/question/pg_repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Repository interface {
-	CreateQuestion(ctx context.Context, quizID int, input domain.CreateQuestion) (int, error)
+	CreateQuestion(ctx context.Context, quizID int) (int, error)
 	GetQuestionsByID(ctx context.Context, id int) ([]models.Question, error)
 	GetQuestionsWithAnswers(ctx context.Context, id int) ([]models.QuestionWithAnswers, error)
 	Update(ctx context.Context, id int, input domain.Question) error
