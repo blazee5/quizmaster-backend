0f54a7e3bc668ed1d5473af96ab4edff5457d7fc
blazee5 <vladislavkutsenko742@gmail.com>
2023-11-28 11:55:35 +0600

    delete required field in question

--- internal/domain/question.go
+++ internal/domain/question.go
@@ -3,7 +3,7 @@ package domain
 type Question struct {
 	Title   string `json:"title"`
 	Type    string `json:"type" validate:"required,oneof=choice input"`
-	OrderID int    `json:"order_id" validate:"required"`
+	OrderID int    `json:"order_id"`
 	QuizID  int
 }
 
